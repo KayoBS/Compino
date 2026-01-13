6e5a1576a350d90900e17bbf845ea48214b11549
KayoBS <kayobs@hotmail.com>
2026-01-13 01:25:20 -0300

    Sofisticação das flags para o comando e possibilidade do uso de outras placas além da Uno

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # COMPINO
-This is a small (really small) library for compiling and sending code to Arduino. Currently, it meets all my needs by doing only the very basics, so much so that the code is quite poor, but I intend to improve it and add other functionalities in the future.
+This is a small command to facilitate to compile and send code to Arduino board family in weak/old computers. Currently, it meets all my needs by doing only the very basics.
+## The computer I'm using
+Currently, I'm using a 2016 Lenovo Ideapad 110 with 3.73 GB of RAM, an Intel Celeron N3060 processor with a 2.48 GHz clock speed, integrated graphics, and running Arch Linux with i3WM as my operating system.
 ## Requirements
-You need to have ```arduino-cli``` installed.
+You need to have ```arduino-cli``` installed and any Linux distribution.
 ## Initial Steps
-First, compile the code with ```make``` and, to make the compiled code a command, use ```make install```.
+First, compile and make the compiled code a command with ```make install```.
 To clear intermediate files, use ```make clean``` and to uninstall the command, use ```make uninstall```.
 ## How to Use
-To open the help tab, type ```compino --help``` or ```compino -h```.
+To open the help guide for informations and examples, type ```compino -h```.
+To check the version, use ```compino -v```
 
-To compile your .ino code for your board, first make sure it is connected and, to be sure which serial port you should use to send the code, use the command ```arduino-cli board list```. Knowing which port to use, type ```compino your_serial_port```, where "your_serial_port" is the port where the Arduino Uno is connected.
-Example: ```compino /dev/ttyUSB0```
+To compile your .ino code for your board, first make sure it is connected and, to be sure which serial port you should use to send the code, use the command ```arduino-cli board list```. Knowing which port to use, type ```compino -s <serial_port> -b <board_name>```, where "serial_port" is the port where the Arduino Uno is connected and "board_name" is the abbreviated name of your board, like "uno", for Arduino Uno, or "mega", for Arduino Mega. For more informations, use the help guide.
+Example: ```compino -s /dev/ttyUSB0 -b uno```
+## Things to add
+I still intend to add colors to the command messages, more sophisticated error messages, and add more commands without compromising the simplicity of this project.
+
--- compino.cpp
+++ compino.cpp
@@ -1,38 +1,105 @@
 #include <iostream>
+#include <bitset>
 #include <string>
+#include <string_view>
+#include <vector>
 
 // compile .ino | compile for arduino
 int main(int argc, char *argv[]) {
 
-  if (argc == 2) {
-    std::string port = argv[1];
-    std::string port_command = port;
+  std::vector<std::string> command_args(argv + 1, argv + argc);
 
-    if (port_command == "--help" || port_command == "-h") {
+  std::string compile_command = {""};
+  std::string upload_command = {""};
 
-      std::cout << "To compile your .ino file, type \"compino your_serial_port\", where \"your_serial_port\" is the port to which your Arduino Uno is connected.To see the connected ports, use the command \"arduino-cli board list\"";
-      std::cout << "\n\n--help       -h    - help guide";
-    }
+  std::string board = {""};
+  std::string serial_port = {""};
 
-    else {
-      port_command =
-          "arduino-cli upload -p " + port + " --fqbn arduino:avr:uno .";
+  std::bitset<8> in_process = {0b00000000};
+  bool compile_try = {false};
 
-      std::cout << "\nCommand port defined";
-
-      std::system("arduino-cli compile --fqbn arduino:avr:uno .");
-      std::cout << "\nCompilation ends";
-
-      std::system(port_command.c_str());
-      std::cout << "\nCode uploaded to serial port " << port;
-    }
+  if( argc < 2 || argc >= 8 ) {
+    std::cout << "Invalid number of arguments! To see how this command works, type: compino -h" << '\n';
   }
-
   else {
-    std::cout << "\nInvalid number of arguments";
+    //Depois tentar implementar um foreach ou uma recursão!
+    for( size_t cont = 0 ; cont < command_args.size() ; ++cont ) {
+
+      std::string_view arg = command_args[cont];
+
+      if(arg == "-h") {
+        //Depois mudar isso para um system(cat ~/etc/bin/helpguide.txt)
+        std::cout << "This is the compino help guide, a command to facilitate compilation for Arduino family boards on weak/old computers. This command is designed to work on Linux distributions." << '\n';
+        std::cout << "-h                        - Help guide" << '\n';
+        std::cout << "-v                        - Version" << '\n';
+        std::cout << "-s <serial port>          - Indicates which serial port the code should be compiled for (serial port where the Arduino is connected). To see the available ports, use: arduino-cli board list" << '\n';
+        std::cout << "-b <board name>           - Indicates which Arduino family model to compile for" << '\n';
+
+        std::cout << "\nExample usage format:" << '\n';
+        std::cout << "compino -s /dev/ttyUSB0 -b uno" << '\n';
+
+        std::cout << "\nSupported boards for compilation:" << '\n';
+        std::cout << "Arduino Uno         -b uno" << '\n';
+        std::cout << "Arduino Mega        -b mega" << '\n';
+        std::cout << "Arduino Micro       -b micro" << '\n';
+        std::cout << "Arduino Nano        -b nano" << '\n';
+        std::cout << "Arduino Leonardo    -b leo" << '\n';
+      }
+
+      else if(arg == "-v") {
+        std::cout << "2026.01.12" << '\n';
+      }
+
+      else if(arg == "-b") {
+        if( (cont+1) < command_args.size() ) {
+          board = command_args[++cont];
+          in_process |= 0b11110000;
+          compile_try = true;
+        }
+        else {
+          std::cout << "You need to specify the Arduino model after the -b flag! For more information and examples, type: compino -h" << '\n';
+        }
+      }
+
+      else if(arg == "-s") {
+        if( (cont+1) < command_args.size() ) {
+          serial_port = command_args[++cont];
+          in_process |= 0b00001111;
+          compile_try = true;
+        }
+        else {
+          std::cout << "You need to specify the serial port after the -s flag! For more information and examples, type: compino -h" << '\n';
+        }
+      }
+
+      else {
+        std::cout << "Flag not found: " << command_args[cont] << '\n';
+        std::cout << "To see the flags that can be used by this command, access the help guide by typing: compino -h" << '\n';
+      }
+
+    }
+
+
+    if( compile_try ) {
+      in_process &= 0b11111111;
+      compile_command = "arduino-cli compile --fqbn arduino:avr:" + board + " .";
+      upload_command = "arduino-cli upload -p " + serial_port + " --fqbn arduino:avr:" + board + " .";
+
+      if( in_process.all() ) {
+   
+        //std::cout << "Compiling sketch..." << '\n';
+        system( compile_command.c_str() );
+        //std::cout << "The sketch has been compiled." << '\n';
+
+        //std::cout << "Sending sketch to the board..." << '\n';
+        system( upload_command.c_str() );
+        //std::cout << "Sketch successfully sent to the board." << '\n';
+
+      }
+      else {
+        std::cout << "To be able to compile, you need the serial port and the Arduino model! For more information and examples, type: compino -h" << '\n';
+      }
+    }
   }
-
-  std::cout << '\n';
-
   return 0;
 }
